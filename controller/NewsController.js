382c6b2db4b5f0f4162ef9f4b7715f188bb905bd
Pavel Goncharov <p.goncharov@dunice.net>
2022-06-14 12:29:46 +0300

    fixed newsController and request to add news

--- controller/NewsController.js
+++ controller/NewsController.js
@@ -4,16 +4,44 @@ module.exports = {
   async selectAll(req, res) {
     try {
       const newsList = await News.findAll({
-        include: [{
-          model: User,
-          as: 'users',
-          attributes: ['id', 'login'],
-        }],
+        include: [
+          {
+            model: User,
+            as: 'users',
+            attributes: ['id', 'login'],
+          },
+        ],
       });
 
       return res.status(200).send(newsList);
     } catch (error) {
       return res.status(500).send({ message: 'Ошибка!' });
+    }
+  },
+  async addNews(req, res) {
+    try {
+      const {
+        body: { title, text, tags },
+      } = req;
+
+      const {
+        dataValues: { id },
+      } = req.user;
+      if (title === '' || title === null || text === '' || text === null) {
+        throw new Error('Обязательное поле не заполненно!');
+      }
+      const newNews = News.create({
+        title,
+        text,
+        id,
+        tags,
+      });
+      newNews.then((result) => res.status(201)
+        .send({
+          message: `Новость ${result.dataValues.title} успешно добавлена!`,
+        }));
+    } catch (errr) {
+      return res.status(501).send({ message: 'Невозможно добавить материал' });
     }
   },
 };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,11 +1,10 @@
 const express = require('express');
 
 const { isAuthenticated } = require('../middleware');
-const { getUserData, addNews } = require('../controller/userController');
+const { getUserData } = require('../controller/userController');
 
 const router = express.Router();
 
 router.get('/:id', isAuthenticated, getUserData);
-router.post('/add', isAuthenticated, addNews);
 
 module.exports = router;
