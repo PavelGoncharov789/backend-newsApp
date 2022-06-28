1f2ccd21eae6f0ee5fe0b6db033cc44fa9e13681
Pavel Goncharov <p.goncharov@dunice.net>
2022-06-28 16:12:28 +0300

    fixed path news image

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/news')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/news', newsRouter);
 app.use('/auth', authRouter);
